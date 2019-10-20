3c0d999723237f88cef769f9f18d80de839ef46c
manukmanch <manukbarseghyan90@gmail.com>
2019-10-20 12:54:45 +0400

    Added Practical1 files to repo

---
+++ week1/Problem2.py
@@ -0,0 +1,12 @@
+course = "Intro to Python"
+student = "Nune Melikyan"
+nickname = "nunemel"
+grade = "100"
+python_expert = False
+
+
+print("course: " + course + "\n"
+      "student: " + student + "\n"
+      "nickname: "  + nickname + "\n"
+      "grade: " + grade + "\n"
+      "python_expert: %s" %python_expert)
---
+++ week1/Problem4.py
@@ -0,0 +1,4 @@
+import math
+AB = 3; AC = 4; BC = 0
+BC = int(math.sqrt(AB**2 + AC**2))
+print("The hypotenuse of the triangle ABC: ",BC)
