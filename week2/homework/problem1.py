54e8213cdd7187598e26102c87e93ff6567a61da
nunemel <nune.melikyan@yahoo.com>
2019-10-27 20:00:08 +0400

    updated problem1.py

--- week2/homework/problem1.py
+++ week2/homework/problem1.py
@@ -1,18 +1,17 @@
 import argparse, datetime
+from dateutil.relativedelta import relativedelta
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--num_y", help = "the number of years", type=int)
 parser.add_argument("--num_d", help = "the number of days", type=int)
 current_day = datetime.datetime.now()
-cuurent_year = current_day.year
 final_date = current_day
 
 args = parser.parse_args()
 str_not_given = "not given"
 if args.num_y:
     years = args.num_y
-    tdelta_years = datetime.timedelta(years=years)
-    final_date = final_date + tdelta_years
+    final_date = final_date + relativedelta(years=years)
 else:
     years = str_not_given    
 if args.num_d: 
@@ -20,7 +19,7 @@ if args.num_d:
     tdelta_days = datetime.timedelta(days=days)   
     final_date = final_date + tdelta_days
 else:
-    tdelta_days = str_not_given
+    days = str_not_given
        
 print("""Current date: %s     
 Given years: %s
